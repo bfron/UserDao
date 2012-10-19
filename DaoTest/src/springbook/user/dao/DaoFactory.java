a05d083cdfa14ee9eab7b339ea3536765b05d24b
B-Fron <gandamiro@gmail.com>
2012-10-20 01:34:49 +0900

    리스트 1-19 애플리케이션 컨텍스트를 적용한 UserDaoTest

--- DaoTest/src/springbook/user/dao/DaoFactory.java
+++ DaoTest/src/springbook/user/dao/DaoFactory.java
@@ -5,12 +5,17 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DaoFactory {
+	@Bean
 	public UserDao userDao() {
 		return new UserDao(connectionMaker());
 	}
 	
+	@Bean
 	public ConnectionMaker connectionMaker() {
 		return new DConnectionMaker();
 	}
