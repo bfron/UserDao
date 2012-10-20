5df11ef6d272877763e99593774b3c287ff68a6b
B-Fron <gandamiro@gmail.com>
2012-10-20 16:05:02 +0900

    리스트 2-14 데이터를 찾지 못하면 예외를 발생시키도록 수정한 get() 메소드

--- DaoTest/src/springbook/user/dao/UserDao.java
+++ DaoTest/src/springbook/user/dao/UserDao.java
@@ -7,6 +7,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 
+import org.springframework.dao.*;
+
 import springbook.user.domain.User;
 
 public class UserDao {
@@ -33,21 +35,25 @@ public class UserDao {
 
 	public User get(String id) throws SQLException {
 		Connection c = this.dataSource.getConnection();
-	
+		
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 
 		ResultSet rs = ps.executeQuery();
-		rs.next();
-		User user = new User();
-		user.setId(rs.getString("id"));
-		user.setName(rs.getString("name"));
-		user.setPassword(rs.getString("password"));
-
+		
+		User user = null;
+		if(rs.next()) {
+			user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+		}
+		
 		rs.close();
 		ps.close();
 		c.close();
-
+		
+		if(user == null) throw new EmptyResultDataAccessException(1);
 		return user;
 	}
 	public void deleteAll() throws SQLException {
